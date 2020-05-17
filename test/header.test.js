8bffa53001fd63143bf250ab9f446a6753cc40f6
kapeiharshad <kapeiharshad@gmail.com>
2020-05-17 23:33:39 +0530

    test header added

--- test/header.test.js
+++ test/header.test.js
@@ -1,17 +1,27 @@
 const puppetter = require("puppeteer")
 
-test("Add two number",()=>{
-    const sum =1+2;
-    expect(sum).toEqual(3);
-});
+let browser,page;
+
+//  runs before each test describe
+beforeEach(async() =>{
+        browser = await puppetter.launch({
+        headless:false
+        })
+         page = await browser.newPage();
+        await page.goto("http://localhost:3000")
+})
+
+afterEach(async()=>{
+    await browser.close();
+})
 
 test("We can launch a bowser",async ()=>{
-        const browser = await puppetter.launch({
-            headless:false
-        })
-        const page = await browser.newPage();
-        await page.goto("http://localhost:3000")
-
-        const text= await page.$eval("a.brand-logo",el=> el.innerHTML)
-        expect(text).toEqual("Blogster")
+    const text= await page.$eval("a.brand-logo",el=> el.innerHTML)
+    expect(text).toEqual("Blogster")
 });
+
+test("click login start oauth flow",async ()=>{
+    await page.click(".right a");
+    const url =await page.url();
+    console.log("from url ",url)
+})
